746c945bb7362cb492b9aec26c92191aad4c2104
mrsimpson-stack <simpsontechnology7@gmail.com>
2025-07-04 10:46:30 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 // Contact details
-global.ownernomer = process.env.OWNER_NOMER || "256708565310";
+global.ownernomer = process.env.OWNER_NOMER || "256749491897";
 global.ytname = process.env.YT_NAME || "YT: @EliteProTech";
 global.socialm = process.env.SOCIAL_M || "GitHub: EliteProTech";
-global.location = process.env.LOCATION || "Nigeria, Port Harcourt";
+global.location = process.env.LOCATION || "Kampala, Namsana";
 
 // Creator details
-global.ownernumber = process.env.OWNER_NUMBER || '256708565310';
+global.ownernumber = process.env.OWNER_NUMBER || '256749491897';
 global.ownername = process.env.OWNER_NAME || 'Simpson Tech';
 global.botname = process.env.BOT_NAME || 'ELITE-PRO-V1';
 
@@ -22,7 +22,7 @@ global.autorecordtype = process.env.AUTO_RECORD_TYPE === 'true';
 global.autoread = process.env.AUTO_READ === 'true';
 global.autobio = process.env.AUTO_BIO !== 'false'; // default true
 global.anti92 = process.env.ANTI_92 === 'true';
-global.autoswview = process.env.AUTO_SW_VIEW !== 'false'; // default true
+global.autoswview = process.env.AUTO_SW_VIEW !== 'true'; // default true
 global.welcome = process.env.WELCOME !== 'false'; // default true
 global.autoreact = process.env.AUTO_REACT === 'true';
 global.autolikestatus = process.env.AUTO_LIKE_STATUS === 'true';
